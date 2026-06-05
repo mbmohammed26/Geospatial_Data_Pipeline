444f79f87ea86a7048fd5106d838cb5e7f895c17
mbmohammed26 <mbmohammed26@users.noreply.github.com>
2026-06-05 17:17:37 +0100

    Remove duplicate geodata database creation from init script

--- db-init/init-databases.sql
+++ db-init/init-databases.sql
@@ -1,9 +1,7 @@
 -- Initialize databases for the services
 CREATE DATABASE airflow;
 CREATE DATABASE superset;
-CREATE DATABASE geodata;
 
 -- Grant privileges (optional but good practice for local admin)
 GRANT ALL PRIVILEGES ON DATABASE airflow TO geo_admin;
 GRANT ALL PRIVILEGES ON DATABASE superset TO geo_admin;
-GRANT ALL PRIVILEGES ON DATABASE geodata TO geo_admin;
