ba26bfd7ebea3570ec8555e1477901500354dd69
mbmohammed26 <mbmohammed26@users.noreply.github.com>
2026-06-05 20:07:48 +0100

    Enable postgis extension during db initialization

--- db-init/init-databases.sql
+++ db-init/init-databases.sql
@@ -5,3 +5,7 @@ CREATE DATABASE superset;
 -- Grant privileges (optional but good practice for local admin)
 GRANT ALL PRIVILEGES ON DATABASE airflow TO geo_admin;
 GRANT ALL PRIVILEGES ON DATABASE superset TO geo_admin;
+
+-- Connect to geodata database and enable PostGIS extension
+\c geodata
+CREATE EXTENSION IF NOT EXISTS postgis;
